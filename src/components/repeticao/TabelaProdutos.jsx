f87ffcf9606635cac5d604ac77f5940e29e52336
Andreza Nóbrega <ancarvalho@ecomp.uefs.br>
2023-01-06 13:55:59 -0300

    Desafio tabela

--- src/components/repeticao/TabelaProdutos.jsx
+++ src/components/repeticao/TabelaProdutos.jsx
@@ -1,29 +1,34 @@
 //criar uma tabela onde mostre os produtos
 // em suas devidas colunas
+import './TabelaProdutos.css'
 import React from "react";
 import produtos from "../../data/produtos";
+
+//to fixed, para programar apenas duas casas decimais
 
 export default props =>{
     const { products } = props;
     return (
-      <table>
-        <caption>Tabela de produtos</caption>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Nome</th>
-            <th>Preço</th>
-          </tr>
-        </thead>
-        <tbody>
-          {produtos.map(produtos => (
-            <tr key={produtos.id}>
-              <td>{produtos.id}</td>
-              <td>{produtos.nome}</td>
-              <td>{produtos.preço}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+        <div className="TabelaProdutos">
+            <table>
+                <caption>Tabela de produtos</caption>
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                </tr>
+                </thead>
+                <tbody>
+                {produtos.map(produtos => (
+                    <tr key={produtos.id}>
+                    <td>{produtos.id}</td>
+                    <td>{produtos.nome}</td>
+                    <td>R$ {produtos.preço.toFixed(2).replace('.', ',')}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
     );
   }
